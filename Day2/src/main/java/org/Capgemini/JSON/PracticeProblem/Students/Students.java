3017471cc9ddf643b32ff59adf2b3b3b09c4c342
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-02-19 15:27:13 +0530

    [Ayush] add . Students JSON

--- Day2/src/main/java/org/Capgemini/JSON/PracticeProblem/Students/Students.java
+++ Day2/src/main/java/org/Capgemini/JSON/PracticeProblem/Students/Students.java
@@ -1,4 +1,4 @@
-package org.Capgemini.JSON.Students;
+package org.Capgemini.JSON.PracticeProblem.Students;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
